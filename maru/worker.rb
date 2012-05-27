0ae640798f435b3695bb90f1dbcbf89e0c5bc03c
devyn <devyn.cairns@gmail.com>
2012-05-27 03:22:11 -0700

    Crappy, but sort of working worker

--- maru/worker.rb
+++ maru/worker.rb
@@ -1,5 +1,11 @@
 require 'json'
-require 'net/http'
+require 'rest_client'
+
+class String
+	def to_class_name
+		capitalize.gsub( /[_ ]([A-Za-z])/ ) { $1.upcase }
+	end
+end
 
 module Maru
 	class Worker
@@ -7,17 +13,78 @@ module Maru
 
 		DEFAULTS = {
 			masters:  [],
+			kinds:    nil,
 			temp_dir: "/tmp/maru.#$$"
 		}
+
+		class NothingToDo < Exception; end
+
+		module Plugins; end
 
 		def initialize( config={} )
 			config = DEFAULTS.dup.merge( config )
 
-			@masters  = config[:masters]
+			@masters  = config[:masters].map { |u| RestClient::Resource.new( u ) }
+			@kinds    = config[:kinds]
 			@temp_dir = config[:temp_dir]
+			@robin    = 0
 		end
 
-		def do_job
+		def work
+			raise NothingToDo if @masters.empty?
+
+			passed = []
+			until passed.include? @robin
+				m = @masters[@robin]
+
+				opt = {}
+				opt[:kinds] = @kinds.join[","] if @kinds
+
+				job = nil
+
+				m['job'].get :params => opt do |response, request, result, &block|
+					case response.code
+					when 200
+						job = JSON.parse( response )["job"]
+
+						process_job job, m
+						return
+					when 503
+						puts "\e[1m> \e[0;34m#{m} has no work for us\e[0m"
+					else
+						response.return! request, result, &block
+					end
+
+					passed << @robin
+					if @robin >= @masters.length - 1
+						@robin = 0
+					else
+						@robin += 1
+					end
+				end
+			end
+
+			raise NothingToDo
+		end
+
+		def process_job job, master
+			handle = master["job"][job["id"]]
+			plugin = Maru::Worker::Plugins::const_get( job["group"]["kind"].to_class_name )
+
+			puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[33mprocessing\e[0m"
+
+			if plugin.respond_to? :process_job
+				begin
+					files = Hash[plugin.process_job( job ).map { |f| [f, File.new( f )] }]
+					puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[32mdone\e[0m"
+
+					handle.post :assigned_id => job["assigned_id"], :files => files
+				rescue Exception
+					puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[31mforfeiting\e[0m"
+					handle["forfeit"].post :assigned_id => job["assigned_id"]
+					raise $!
+				end
+			end
 		end
 	end
 end
