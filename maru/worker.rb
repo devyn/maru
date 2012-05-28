822086b49080ff31a8f552669da2cd27f13c6a38
devyn <devyn.cairns@gmail.com>
2012-05-27 18:16:32 -0700

    Downloads and verifies prerequisites, chdirs to temp dir

--- maru/worker.rb
+++ maru/worker.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'yaml'
 require 'rest_client'
+require 'fileutils'
+require 'digest'
 
 class String
 	def to_class_name
@@ -10,7 +12,7 @@ end
 
 module Maru
 	class Worker
-		attr_accessor :masters
+		attr_accessor :masters, :kinds, :temp_dir
 
 		DEFAULTS = {
 			masters:  [],
@@ -18,7 +20,8 @@ module Maru
 			temp_dir: "/tmp/maru.#$$"
 		}
 
-		class NothingToDo < Exception; end
+		class NothingToDo  < Exception; end
+		class Inconsistent < Exception; end
 
 		module Plugins; end
 
@@ -45,43 +48,52 @@ module Maru
 
 				job = nil
 
-				m['job'].get :params => opt do |response, request, result, &block|
-					case response.code
-					when 200
-						begin
-							job = JSON.parse( response )["job"]
+				begin
+					m['job'].get :params => opt do |response, request, result, &block|
+						case response.code
+						when 200
+							begin
+								job = JSON.parse( response )["job"]
 
-							puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[33mprocessing\e[0m"
+								puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[33mprocessing\e[0m"
 
-							process_job job, m
-							return
-						rescue Exception
-							puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[31mforfeiting:\e[0m #$!"
-							m['job'][job['id']]["forfeit"].post :assigned_id => job["assigned_id"]
+								with_group job["group"] do
+									process_job job, m
+								end
+								return
+							rescue Exception
+								puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[31mforfeiting:\e[0m #$!"
+								m['job'][job['id']]["forfeit"].post :assigned_id => job["assigned_id"]
 
-							puts "\e[1m> \e[0;34mBlacklisting ##{job["id"]}.\e[0m"
-							@blacklist[m.to_s] ||= []
-							@blacklist[m.to_s] << job["id"]
+								puts "\e[1m> \e[0;34mBlacklisting ##{job["id"]}.\e[0m"
+								@blacklist[m.to_s] ||= []
+								@blacklist[m.to_s] << job["id"]
 
-							raise $!
+								raise $!
+							end
+						when 503
+							#puts "\e[1m> \e[0;34m#{m} has no work for us\e[0m"
+						else
+							response.return! request, result, &block
 						end
-					when 503
-						#puts "\e[1m> \e[0;34m#{m} has no work for us\e[0m"
-					else
-						response.return! request, result, &block
 					end
+				rescue Errno::ECONNREFUSED
+					warn "\e[1m> \e[0;33mWarning: #{m} may be down.\e[0m"
+				rescue Exception
+				end
 
-					passed << @robin
-					if @robin >= @masters.length - 1
-						@robin = 0
-					else
-						@robin += 1
-					end
+				passed << @robin
+				if @robin >= @masters.length - 1
+					@robin = 0
+				else
+					@robin += 1
 				end
 			end
 
 			raise NothingToDo
 		end
+
+		private
 
 		def process_job job, master
 			handle = master["job"][job["id"]]
@@ -89,12 +101,62 @@ module Maru
 
 			if plugin.respond_to? :process_job
 				files = Hash[plugin.process_job( job ).map { |f| [f, File.new( f )] }]
-				puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[32mdone\e[0m"
+
+				puts "\e[1m  > \e[0;34mUploading results...\e[0m"
 
 				handle.post :assigned_id => job["assigned_id"], :files => files
+
+				files.each do |f|
+					File.unlink f[0]
+				end
+
+				puts "\e[1m> ##{job["id"]} (#{job["group"]["name"]}) \e[32mdone\e[0m"
 			end
 		end
 
+		def with_group group
+			FileUtils.mkdir_p( path = File.join( @temp_dir, "group-#{group["id"]}" ) )
+
+			Dir.chdir path do
+				group["prerequisites"].each do |pre|
+					next unless verify_path( pre["destination"] )
+
+					if File.file? pre["destination"]
+						next if check_consistency pre["destination"], pre["sha256"]
+					end
+
+					puts "\e[1m  > \e[0;34mDownloading #{pre["destination"]} from #{pre["source"]}...\e[0m"
+
+					RestClient::Resource.new( pre["source"], :raw_response => true ).get do |response,request,result,&block|
+						if response.code == 200
+							File.open pre["destination"], 'w' do |f|
+								IO::copy_stream response.file, f
+							end
+
+							if check_consistency( pre["destination"], pre["sha256"] ) == false
+								raise Inconsistent, "#{pre["destination"]} does not match the checksum."
+							end
+						else
+							response.return! request, result, &block
+						end
+					end
+				end
+
+				yield
+			end
+		end
+
+		def check_consistency path, sha256
+			puts "\e[1m  > \e[0;34mChecking #{path} for consistency...\e[0m"
+
+			sha256 == Digest::SHA256.file( path ).hexdigest
+		end
+
+		def verify_path path
+			File.expand_path( path )[0, Dir.pwd.size] == Dir.pwd
+		end
+
+		public
 
 		def self.run
 			require 'optparse'
@@ -114,6 +176,14 @@ module Maru
 
 				opts.on "-t", "--temp-dir DIR", "Specify directory to use for temporary files. Default: /tmp/maru.<PID>" do |temp_dir|
 					options[:temp_dir] = temp_dir
+				end
+
+				opts.on "--[no-]keep-temp", "Keep temp dir on exit. Default: false" do |v|
+					options[:keep_temp] = v
+				end
+
+				opts.on "-w", "--wait-time N", "Amount of time to wait (in seconds) if there are no jobs available. Default: 30" do |num|
+					options[:wait_time] = num.to_i
 				end
 
 				opts.on "-r", "--require FILE", "Load a ruby script (most likely a plugin) before starting" do |f|
@@ -142,12 +212,18 @@ module Maru
 				exit
 			end
 
+			at_exit do
+				unless options[:keep_temp]
+					FileUtils.rm_rf w.temp_dir
+				end
+			end
+
 			loop do
 				begin
 					w.work
 				rescue Maru::Worker::NothingToDo
-					puts "\e[1m> \e[0;34mNothing to do. Waiting 30 seconds.\e[0m"
-					sleep 30
+					puts "\e[1m> \e[0;34mNothing to do. Waiting #{options[:wait_time] || 30} seconds.\e[0m"
+					sleep options[:wait_time] || 30
 				rescue SystemExit
 					raise $!
 				rescue Exception
