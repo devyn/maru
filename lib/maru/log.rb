b969d6507479c897710ceb468ffaf664c1c4570b
Devyn Cairns <devyn.cairns@gmail.com>
2013-07-06 02:41:45 -0700

    log timestamps

--- lib/maru/log.rb
+++ lib/maru/log.rb
@@ -1,36 +1,40 @@
 module Maru
   class Log
-      def initialize(out, color=nil)
-        @out   = out
-        @color = color.nil? ? out.tty? : color
+    def initialize(out, color=nil)
+      @out   = out
+      @color = color.nil? ? out.tty? : color
+    end
+
+    def info(msg, options={})
+      if @color
+        @out << "\e[34m[#{time}]\e[0;1m>> \e[0;36m#{msg}\e[0m"
+      else
+        @out << "[#{time}]>> #{msg.gsub(/\e\[\d{1,2}(?:;\d{1,2})?m/, '')}"
       end
 
-      def info(msg, options={})
-        if @color
-          @out << "\e[1m>> \e[0;36m#{msg}\e[0m"
-        else
-          @out << ">> #{msg.gsub(/\e\[\d{1,2}(?:;\d{1,2})?m/, '')}"
-        end
+      if options[:newline] == false
+        @out.flush
+      else
+        @out << "\n"
+      end
+    end
 
-        if options[:newline] == false
-          @out.flush
-        else
-          @out << "\n"
-        end
+    def warn(msg, options={})
+      if @color
+        @out << "\e[34m[#{time}]\e[0;1m!! \e[0;33m#{msg}\e[0m"
+      else
+        @out << "[#{time}]!! #{msg.gsub(/\e\[\d{1,2}(?:;\d{1,2})?m/, '')}"
       end
 
-      def warn(msg, options={})
-        if @color
-          @out << "\e[1m!! \e[0;33m#{msg}\e[0m"
-        else
-          @out << "!! #{msg.gsub(/\e\[\d{1,2}(?:;\d{1,2})?m/, '')}"
-        end
-
-        if options[:newline] == false
-          @out.flush
-        else
-          @out << "\n"
-        end
+      if options[:newline] == false
+        @out.flush
+      else
+        @out << "\n"
       end
+    end
+
+    def time
+      Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+    end
   end
 end
